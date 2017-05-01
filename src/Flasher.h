b2efddcf85caa51405a7013d26d283a64751eba1
Bart Dupon <b@barrit.be>
2017-05-02 00:06:38 +0200

    heartbeat and sonos talker

---
+++ src/Flasher.h
@@ -0,0 +1,5 @@
+class Flasher {
+public:
+  Flasher(int pin, long on, long off);
+  void Update();
+};
--- src/PushIt.ino
+++ src/PushIt.ino
@@ -1,33 +1,42 @@
 #include "HttpClient.h"
+#include "Flasher.h"
 
- int LED = D7;
- bool on = false;
- unsigned int nextTime = 0;
+Flasher heartbeat(D7, 100, 1900);  //flash onboard led
 
- HttpClient http;
+int stereo = D0;
+int button = D1;
 
- void setup() {
-   pinMode(LED, OUTPUT);
+HttpClient http;
 
- }
+void setup() {
+  pinMode(stereo, INPUT_PULLUP);
+  pinMode(button, INPUT_PULLUP);
+}
 
- void loop() {
-   if (nextTime > millis()) {
-       return;
-   }
-   else
-   {
-       if (on)
-       {
-           digitalWrite(LED, LOW);
-           on = false;
-       }
-       else
-       {
-           digitalWrite(LED, HIGH);
-           on = true;
-       }
-   }
-   nextTime = millis() + 3000;
+void loop() {
+  if (digitalRead(stereo) == LOW) {
+    // switch = on
+    heartbeat.Update();
+  }
 
- }
+  if (digitalRead(button) == LOW) {
+    SonosTalk("Hello world", "en-us", 20);
+  }
+
+}
+
+void SonosTalk(String what, String language, int volume) {
+
+  http_header_t headers[] = {
+    { "Accept" , "*/*"},
+    { NULL, NULL } // NOTE: Always terminate headers will NULL
+  };
+
+  http_request_t request;
+  http_response_t response;
+
+  request.hostname = "192.168.0.99";
+  request.port = 5005;
+  request.path = String("/office/say/"+ what.replace(" ", "%20") + "/" + language + "/" + volume);
+  http.get(request, response, headers);
+}
